76f6c65b2790249911f106b9dd063138a131fd2f
oliveiraxand <alexandre.osantos253@gmail.com>
2024-01-20 09:14:04 -0300

    feat: create model, request and controller for EnterpriseController

---
+++ phill/app/Http/Controllers/EnterpriseController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Enterprise;
+use Illuminate\Http\Request;
+use App\Http\Requests\EnterpriseRequest;
+
+class EnterpriseController extends Controller
+{
+    
+}
