40fd604b371c496e4607ba069e0ba62de68c1ac5
oliveiraxand <alexandre.osantos253@gmail.com>
2024-01-28 07:43:07 -0300

    feat: create relationships

--- phill/app/Models/Visit.php
+++ phill/app/Models/Visit.php
@@ -10,4 +10,21 @@ class Visit extends Model
     use HasFactory;
     protected $table = 'visits';
     protected $fillable = ['person_id', 'vehicle_id', 'arrival', 'input', 'output', 'enterprise_client_id', 'enterprise_partner_id', 'operation_id', 'nfs', 'comment'];
+
+    //Create relationships
+
+    public function person()
+    {
+        return $this->belongsTo(Person::class);
+    }
+
+    public function vehicle()
+    {
+        return $this->belongsTo(Vehicle::class);
+    }
+
+    public function operation()
+    {
+        return $this->belongsTo(Operation::class);
+    }
 }
